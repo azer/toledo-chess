bdd5d8a747742c3785d43b7c6856540e5d3610dc
E.Azer Koçulu <azer@kodfabrik.com>
2011-05-12 00:16:43 +0300

    v2.2 update

--- toledo-chess.js
+++ toledo-chess.js
@@ -1,76 +1,113 @@
 // Javascript chess engine (c)2011 Oscar Toledo G.
 var toledoChess = (function(exports,undefined){
 
-  var B, I,
-      b, i, l, u, x, y, z;
+  var B, i, y, u, b, I = [],
+      G = 120,
+      x = 10,
+      z = 15,
+      M = 1e4,
+      l = [5, 3, 4, 6, 2, 4, 3, 5, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 13, 11, 12, 14, 10, 12, 11, 13, 0, 99, 0, 306, 297, 495, 846, -1, 0, 1, 2, 2, 1, 0, -1, -1, 1, -10, 10, -11, -9, 9, 11, 10, 20, -9, -11, -10, -20, -21, -19, -12, -8, 8, 12, 19, 21];
 
-  for (B = i = y = u = b = i = 5 - 5, x = 10, z = 15, I = [], l = []; l[B] = "ustvrtsuqqqqqqqqyyyyyyyy}{|~z|{}@G@TSb~?A6J57IKJT576,+-48HLSUmgukgg OJNMLK  IDHGFE".charCodeAt(B) - 64, B++ < 120; I[B - 1] = B % x ? B / x % x < 2 | B % x < 2 ? 7 : B / x & 4 ? 0 : l[u++] : 7);
-
-  function X(c, h, e, S, s) {
-    c ^= 8;
-    for (var T, o, L, E, D, O = 20, G, N = -1e8, n, g, d = S && X(c, 0) > 1e4, C, R, A, K = 78 - h << 9, a = c ? x : -x; ++O < 99;) if ((o = I[T = O]) && (G = o & z ^ c) < 7) {
-      A = G-- & 2 ? 8 : 4;
-      C = 9 - o & z ? l[61 + G] : 49;
-
-      do {
-        R = I[T += l[C]];
-        g = D = G | T + a - e ? 0 : e;
-        if (!R && (G || A < 3 || g) || (1 + R & z ^ c) > 9 && G | A > 2) {
-          if (!(2 - R & 7)) return K;
-          for (E = n = G | I[T - a] - 7 ? o & z : 6 ^ c; E; E = !E && !d && !(g = T, D = T < O ? g - 3 : g + 2, I[D] < z | I[D + O - T] | I[T += T - O])) {
-            L = (R && l[R & 7 | 32] * 2 - h - G) + (G ? 0 : n - o & z ? 110 : (D && 14) + (A < 2) + 1);
-            if (S > h || 1 < S & S == h && L > 2 | d) {
-              I[T] = n, I[g] = I[D], I[O] = D ? I[D] = 0 : 0;
-              L -= X(c, h + 1, E = G | A > 1 ? 0 : T, S, L - N);
-              
-              if (!(h || S - 1 | B - O | T - b | L < -1e4)){ 
-                exports.makeMove.callback && ( exports.makeMove.callback(O,T), ( exports.makeMove.callback = undefined ) );
-                W(y = E);
-                return c && setTimeout(function(){ toledoChess.X(8,0,toledoChess.y(),2),toledoChess.X(8,0,toledoChess.y(),1) }, 75);
+  function X(w, c, h, e, S, s) {
+    var t, o, L, E, d, O = e,
+        N = -M * M,
+        K = 78 - h << x,
+        p, g, n, m, A, q, r, C, J, a = y ? -x : x,
+        from, to;
+    y ^= 8;
+    G++;
+    d = w || s && s >= h && X(0, 0, 0, 21, 0, 0) > M;
+    do {
+      if (o = I[p = O]) {
+        q = o & z ^ y;
+        if (q < 7) {
+          A = q-- & 2 ? 8 : 4;
+          C = o - 9 & z ? [53, 47, 61, 51, 47, 47][q] : 57;
+          do {
+            r = I[p += l[C]];
+            if (!w | p == w) {
+              g = q | p + a - S ? 0 : S;
+              if (!r & ( !! q | A < 3 || !! g) || (r + 1 & z ^ y) > 9 && q | A > 2) {
+                if (m = !(r - 2 & 7)) return y ^= 8, I[G--] = O, K;
+                J = n = o & z;
+                E = I[p - a] & z;
+                t = q | E - 7 ? n : (n += 2, 6 ^ y);
+                while (n <= t) {
+                  L = r ? l[r & 7 | 32] - h - q : 0;
+                  if (
+                  s) L += (1 - q ? l[(p - p % x) / x + 37] - l[(O - O % x) / x + 37] + l[p % x + 38] * (q ? 1 : 2) - l[O % x + 38] + (o & 16) / 2 : !! m * 9) + (!q ? !(I[p - 1] ^ n) + !(I[p + 1] ^ n) + l[n & 7 | 32] - 99 + !! g * 99 + (A < 2) : 0) + !(E ^ y ^ 9);
+                  if (s > h || 1 < s & s == h && L > z | d) {
+                    I[p] = n, I[O] = m ? (I[g] = I[m], I[m] = 0) : g ? I[g] = 0 : 0;
+                    L -= X(s > h | d ? 0 : p, L - N, h + 1, I[G + 1], J = q | A > 1 ? 0 : p, s);
+                    if (!(h || s - 1 | B - O | i - n | p - b | L < -M)){ 
+                      exports.makeMove.callback && ( exports.makeMove.callback(O,p) );
+                      return W(), G--, u = J;
+                    }
+                    J = q - 1 | A < 7 || m || !s | d | r | o < z || X(0, 0, 0, 21, 0, 0) > M;
+                    I[O] = o;
+                    I[p] = r;
+                    m ? (I[m] = I[g], I[g] = 0) : g ? I[g] = 9 ^ y : 0;
+                  }
+                  if (L > N || s > 1 && L == N && !h && Math.random() < .5) {
+                    I[G] = O;
+                    if (s > 1) {
+                      if (h && c - L < 0) return y ^= 8, G--, L;
+                      if (!h) i = n, B = O, b = p;
+                    }
+                    N = L;
+                  }
+                  n += J || (g = p, m = p < O ? g - 3 : g + 2, I[m] < z | I[
+                  m + O - p] || I[p += p - O]) ? 1 : 0;
+                }
               }
-
-              E = 1 - G | A < 7 | D | !S | R | o < z || X(c, 0) > 1e4;
-              I[O] = o;
-              I[T] = R;
-              I[D] = I[g];
-              D ? I[g] = G ? 0 : 9 ^ c : 0
             }
-            if (L > N || !h & L == N && Math.random() < .5) if (N = L, S > 1) if (h ? s - L < 0 : (B = O, b = T, 0)) return N
-          }
+          } while (!r & q > 2 || (p = O, q | A > 2 | o > z & !r && ++C * --A));
         }
-      } while (!R & G > 2 || (T = O, G | A > 2 | z < o & !R && ++C * --A))
-    }
+      }
+    } while (++O > 98 ? O = 20 : e - O);
+    return y ^= 8, G--, N + M * M && N > -K + 1924 | d ? N : 0;
+  }
 
-    return -K + 768 < N | d && N
+  function O(){
+    B = i = y = u = 0;
+    while (B++ < 120) I[B - 1] = B % x ? B / x % x < 2 | B % x < 2 ? 7 : B / x & 4 ? 0 : l[i++] | 16 : 7;
+    for (var a = "<table cellspacing=0 align=center border=0>", B = 0; B < 8; B++)
+    for (a += "<tr>", i = 21; i < 29; i++) a += "<td width=40 height=40 onclick=toledoChess.Y(" + (B * x + i) + ") id=o" + (B * x + i) + " style='border:2px solid #e0e0f0' bgcolor=#" + (i + B & 1 ? "f0f" : "c0c") + "0f0 align=center>";
+    a += "<tr><td colspan=8 align=center><select id=t><option>Q<option>R<option>B";
+    //document.write(a + "<option>N</select></table>");
   }
 
   function W() {
-    i = "<table>";
-    var buff;
-    for (u = 18; u < 98;){ 
-      buff = i += ++u % x - 9 ? "<th width=60 height=60 onclick='toledoChess.pI(" + u + ")&8?toledoChess.W():toledoChess.X(0,0,toledoChess.y(),1)'style='font-size:50px'bgcolor=#" + (u - B ? u * .9 & 1 || 9 : "d") + "0f0e0>&#" + (I[u] & 15 ? 9808 + l[67 + (I[u] & 15)] : 160) + ";" : u++ && "<tr>";
-      B = b
+    B = b;
+    /*
+    for (var p = 21; p < 99; p += p % x - 8 ? 1 : 3) {
+      document.getElementById("o" + p).
+      innerHTML = "\xa0pknbrq  PKNBRQ".charAt(I[p] & z);
+      document.getElementById("o" + p).
+      style.borderColor = p == B ? "red" : "#e0e0f0";
+    }*/
+  }
+  O();
+  W();
 
-      if(!W.disabled){
-        document.body.innerHTML = buff;
+  function Y(s) {
+    i = (I[s] ^ y) & z;
+    if (i > 8) {
+      b = s;
+      W();
+    } else if (B && i < 9) {
+      b = s;
+      i = I[B] & z;
+      if ((i & 7) == 1 & (b < 29 | b > 90)) i = 14 - document.getElementById("t").selectedIndex ^ y;
+      X(0, 0, 0, 21, u, 1);
+      if (y){ 
+        setTimeout(function(){
+          X(0,0,0,21,u,2/*ply*/); 
+          X(0,0,0,21,u,1);
+        },250);
+        //window.setTimeout("X(0,0,0,21,u,2/*ply*/),X(0,0,0,21,u,1)", 250);
       }
     }
-  }
-
-  exports.X = X;
-  exports.W = W;
-
-  exports.I = function(){
-    return I;
-  }
-
-  exports.pI = function(nb){
-    nb != undefined && ( b = nb );
-    return I[b];
-  };
-
-  exports.y = function(){
-    return y;
   }
 
   exports.makeMove = (function(){
@@ -81,8 +118,7 @@ var toledoChess = (function(exports,undefined){
       var a = toLoc(from),
           b = toLoc(to);
 
-      exports.pI(toLoc(from)); 
-      W();
+      Y(toLoc(from)); 
 
       setTimeout(function(){
         callback && ( _.callback = function(from,to){
@@ -90,8 +126,7 @@ var toledoChess = (function(exports,undefined){
         });
       },0);
 
-      exports.pI(toLoc(to)); 
-      X(0,0,y,1);
+      Y(toLoc(to)); 
     }
 
     function toLoc(san){
@@ -111,6 +146,9 @@ var toledoChess = (function(exports,undefined){
 
     return _;
   })();
+
+  exports.X = X;
+  exports.Y = Y;
 
   return exports;
 
